ec6100a4ad4f669318d9f8d7105c089ae63eef78
robertolago <robertolagob@gmail.com>
2018-05-01 09:43:18 -0500

    Exercice 2: printing from 4 to 65536 using as potency operation looping 5 times using a do-while

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,11 +1,14 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
 
-    int counter=5;
-    while (counter<=15){
-        System.out.println(counter);
-        counter++;
-    }
+        int number=2;
+        int contador=1;
+    do{
+        System.out.println(number*number);
+        number*=number;
+        contador++;
 
+    }
+    while (contador<5);
     }
 }
