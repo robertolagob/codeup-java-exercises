706dfd36e595a3054482ce7f7890bcab5136dec7
robertolago <robertolagob@gmail.com>
2018-05-01 09:58:43 -0500

    Exercice 2:the "FizzBuzz" using a for loop

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,13 +1,19 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
 
-        int number = 2;
-
-        for (int i = 1; i < 5; i++) {
-            System.out.println(number * number);
-            number *= number;
-
-
+        for (int i = 1; i <= 100; i++) {
+            if ((i % 3 == 0)&&(i%5!=0)) {
+                System.out.println("Fizz");
+            }
+            else if ((i%5==0)&&(i % 3 != 0)){
+                System.out.println("Buzz");
+            }
+            else if((i%5==0)&&(i % 3 == 0)){
+                System.out.println("FizzBuzz");
+            }
+            else {
+                System.out.println(i);
+            }
         }
     }
 
