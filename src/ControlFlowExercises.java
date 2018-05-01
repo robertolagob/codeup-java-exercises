42e0765cfcd5d2392baa73ac1da89ca17020cb11
robertolago <robertolagob@gmail.com>
2018-05-01 09:45:34 -0500

    Exercice 2: printing from 4 to 65536 using a potency operation, looping 5 times using a for loop

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,14 +1,14 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
 
-        int number=2;
-        int contador=1;
-    do{
-        System.out.println(number*number);
-        number*=number;
-        contador++;
+        int number = 2;
 
+        for (int i = 1; i < 5; i++) {
+            System.out.println(number * number);
+            number *= number;
+
+
+        }
     }
-    while (contador<5);
-    }
+
 }
