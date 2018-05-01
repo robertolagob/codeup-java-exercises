ebba4218e35e1a091c00d0f70718762efc79b42c
robertolago <robertolagob@gmail.com>
2018-05-01 09:31:12 -0500

    Exercice 1: printing 5 to 15 numbers

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
-
 public class ControlFlowExercises {
+    public static void main(String[] args) {
+
+    int counter=5;
+    while (counter<=15){
+        System.out.println(counter);
+        counter++;
+    }
+
+    }
 }
