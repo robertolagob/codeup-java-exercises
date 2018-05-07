6744b00a1f1450b922b05e37552ebb382811b9c0
robertolago <robertolagob@gmail.com>
2018-05-07 11:38:18 -0500

    Methods exercises using recursion

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
-
 public class MethodsExercises {
+
+    static public int multiRecursion(int multiplicand,int multiplier){
+
+        if (multiplier==1){
+            return multiplicand;
+        }
+        return multiplicand+multiRecursion(multiplicand,multiplier-1);
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(multiRecursion(4, 4));
+    }
 }
